a1bd1b91402558fa653f21b06a719d97dfa6d578
Oscar Moore <oscar@moore-imac-2015.local>
2021-11-26 15:52:51 +1000

    Basic Animations and trash implemented

--- SEAJAM/Assets/Scripts/FishSpawnHandle.cs
+++ SEAJAM/Assets/Scripts/FishSpawnHandle.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FishSpawnHandle : MonoBehaviour
 {
 
     [SerializeField] private List<GameObject> Trash;
     [SerializeField] private GameObject TrashToSpawn = null;
+    
+    [SerializeField] private Transform MinSpawnHeight;
+    [SerializeField] private Transform MaxSpawnHeight;
     // Start is called before the first frame update
     void Start()
     {
         ChoseRandom();
-    }
+    }   
 
     // Update is called once per frame
     void Update()
@@ -19,6 +24,7 @@ public class FishSpawnHandle : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ChoseRandom();
+            Instantiate(TrashToSpawn, new Vector3(-9f, Random.Range(MinSpawnHeight.position.y, MaxSpawnHeight.position.y), 0), quaternion.identity);
         }
     }
 
